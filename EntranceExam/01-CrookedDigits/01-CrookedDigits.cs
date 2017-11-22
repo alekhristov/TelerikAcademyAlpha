3f354f094f608f6acb6adb13092c24961fd2e513
Alek Hristov <alek.hristov@yahoo.com>
2017-11-22 18:32:45 +0200

    First commit

---
+++ EntranceExam/01-CrookedDigits/01-CrookedDigits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_CrookedDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int num;
+            int sum = 0;
+
+            while (true)
+            {
+                foreach (var digit in input)
+                {
+                    if (int.TryParse(digit.ToString(), out num))
+                    {
+                        sum += num;
+                    }
+                }
+
+                if (sum < 10)
+                {
+                    break;
+                }
+                input = sum.ToString();
+                sum = 0;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ EntranceExam/03-CrookedWalls/03-CrookedWalls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CrookedWalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(a => long.Parse(a)).ToArray();
+            var listResult = new List<long>();
+            long evenSum = 0;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                var greaterNumber = Math.Max(input[i], input[i - 1]);
+                var smallerNumber = Math.Min(input[i], input[i - 1]);
+                var difference = Math.Abs(greaterNumber - smallerNumber);
+
+                if (difference % 2 != 0)
+                {
+                    if (i == 1)
+                    {
+                        listResult.Add(input[i - 1]);
+                        listResult.Add(input[i]);
+                    }
+                    else
+                    {
+                        listResult.Add(input[i]);
+                    }
+                }
+                else
+                {
+                    evenSum += difference;
+                    i++;
+                }
+            }
+            Console.WriteLine(evenSum);
+        }
+    }
+}
