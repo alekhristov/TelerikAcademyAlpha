341b8ecc49db397dbfe58ac16d250a4853eaf868
Alek Hristov <alek.hristov@yahoo.com>
2017-11-25 00:34:30 +0200

    First two problems solved

---
+++ MultidimensionalArrays/02-MaximumSum/02-MaximalSum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_MaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split().ToArray();
+            var n = int.Parse(size[0]);
+            var m = int.Parse(size[1]);
+            var matrix = new int[n, m];
+
+            var maxSum = int.MinValue;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var inputRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int currentSum =
+                        matrix[row, col] +
+                        matrix[row, col + 1] +
+                        matrix[row, col + 2] +
+                        matrix[row + 1, col] +
+                        matrix[row + 1, col + 1] +
+                        matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] +
+                        matrix[row + 2, col + 1] +
+                        matrix[row + 2, col + 2];
+
+                    if (maxSum < currentSum)
+                    {
+                        maxSum = currentSum;
+                    }
+                }
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
