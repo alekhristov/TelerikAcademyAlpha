27c7cd46acc48a7f37168e4e91dfecb9e01c1ea1
Alek Hristov <alek.hristov@yahoo.com>
2017-11-25 11:48:47 +0200

    CorrectBrackets solution

---
+++ 02-Strings/03-CorrectBracketsTests/02-CorrectBracketsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CorrectBracketsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var openBracketCount = 0;
+            var closeBracketCount = 0;
+
+            foreach (var item in input)
+            {
+                if (item == '(')
+                {
+                    openBracketCount++;
+                }
+                else if (item == ')')
+                {
+                    closeBracketCount++;
+                }
+            }
+
+            if (closeBracketCount != openBracketCount)
+            {
+                Console.WriteLine("Incorrect");
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+        }
+    }
+}
