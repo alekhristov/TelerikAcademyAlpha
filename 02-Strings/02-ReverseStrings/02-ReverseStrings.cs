ffd4d9db60a1c8cc797bf9d7e580ee24058bac77
Alek Hristov <alek.hristov@yahoo.com>
2017-11-25 11:37:45 +0200

    ReverseString solution

---
+++ 02-Strings/02-ReverseStrings/02-ReverseStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            Array.Reverse(cArray);
+            text = new string(cArray);
+
+            Console.WriteLine(text);
+
+            //for (int i = cArray.Length - 1; i >= 0; i--)
+            //{
+            //    reverse += cArray[i];
+            //}
+            //Console.WriteLine(reverse);
+
+        }
+    }
+}
