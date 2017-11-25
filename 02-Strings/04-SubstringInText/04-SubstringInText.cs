d84c8a1c7fb652ddecc6d61cf3f7898cb0a753d5
Alek Hristov <alek.hristov@yahoo.com>
2017-11-25 12:19:41 +0200

    SubstringInText solution

---
+++ 02-Strings/04-SubstringInText/04-SubstringInText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04_SubstringInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = Console.ReadLine().ToLower();
+            var text = Console.ReadLine().ToLower();
+            var counter = 0;
+
+            //for (int i = 0; i < text.Length - pattern.Length + 1; i++)
+            //{
+            //    if (text.Substring(i, pattern.Length) == pattern)
+            //    {
+            //        counter++;
+            //        i += pattern.Length - 1;
+            //    }
+            //}
+            var start = 0;
+
+            while (true)
+            {
+                if (text.Substring(start, pattern.Length) == pattern)
+                {
+                    counter++;
+                    start += pattern.Length;
+                }
+                else
+                {
+                    start++;
+                }
+
+                if (start + pattern.Length > text.Length)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
