6e34778fc80e231829db7a7eae23e39bf0c03f94
Alek Hristov <alek.hristov@yahoo.com>
2017-11-26 10:45:32 +0200

    ParseUrls two solutions added

---
+++ 02-Strings/07-ExtractSentences/08-ExtractSentences.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07_ExtractSentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var input = Console.ReadLine().Split('.');
+            var result = new List<string>();
+            var pattern = @"\b" + word + @"\b";
+
+            foreach (var sentence in input)
+            {
+                var matchedSentence = Regex.Match(sentence, pattern);
+                if (matchedSentence.Success)
+                {
+                    result.Add(sentence.Trim());
+                }
+            }
+            if (result.Count > 1)
+            {
+                Console.WriteLine(string.Join(". ", result) + ".");
+            }
+            else if(result.Count == 1)
+            {
+                Console.WriteLine(result[0] + '.');
+            }
+        }
+    }
+}
---
+++ 02-Strings/12-ParseURL/12-ParseURL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _12_ParseURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var pattern = @"^(?<protocol>.*)://(?<server>.*?)(?<resource>/.*)$";
+
+            var matchedUrl = Regex.Match(input, pattern, RegexOptions.Multiline);
+            if (matchedUrl.Success)
+            {
+                Console.WriteLine("[protocol] = {0}", matchedUrl.Groups["protocol"].Value);
+                Console.WriteLine("[server] = {0}", matchedUrl.Groups["server"].Value);
+                Console.WriteLine("[resource] = {0}", matchedUrl.Groups["resource"].Value);
+            }
+
+            //Solution using String class only
+            //var url = Console.ReadLine();
+            //var protocol = url.Substring(0, url.IndexOf(':'));
+
+            //url = url.Remove(0, url.IndexOf(':') + 3);
+            //var server = url.Substring(0, url.IndexOf('/'));
+
+            //url = url.Remove(0, url.IndexOf('/'));
+            //var resource = url;
+
+            //Console.WriteLine("[protocol] = {0}", protocol);
+            //Console.WriteLine("[server] = {0}", server);
+            //Console.WriteLine("[resource] = {0}", resource);
+        }
+    }
+}
