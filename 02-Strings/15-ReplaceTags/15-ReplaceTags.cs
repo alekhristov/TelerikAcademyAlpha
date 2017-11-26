2d105400513d3a3b49533b4d6b70c7fb125675d0
Alek Hristov <alek.hristov@yahoo.com>
2017-11-27 01:16:12 +0200

    ReplaceTags great solution by Irina

--- 02-Strings/15-ReplaceTags/15-ReplaceTags.cs
+++ 02-Strings/15-ReplaceTags/15-ReplaceTags.cs
@@ -7,28 +7,33 @@ namespace _15_ReplaceTags
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
 
-            while (true)
-            {
-                if (!input.Contains(@"<a href="))
-                {
-                    Console.WriteLine(input);
-                    break;
-                }
-                var tagStartIndex = input.IndexOf(@"<a href=");
-                var tagEndIndex = input.IndexOf(@"</a>") + 4;
-                var tag = input.Substring(tagStartIndex, tagEndIndex - tagStartIndex);
+            string text = Console.ReadLine();
+            string parsedHTML = Regex.Replace(text, "(<a href=\")(.*?)(\">)(.*?)(</a>)", "[$4]($2)");
+            Console.WriteLine(parsedHTML);
 
-                var urlStartIndex = input.IndexOf(@"<a href=") + 9;
-                var urlEndIndex = input.IndexOf(@""">");
-                var url = input.Substring(urlStartIndex, urlEndIndex - urlStartIndex);
+            //var input = Console.ReadLine();
 
-                var textStartIndex = urlEndIndex + 2;
-                var textEndIndex = tagEndIndex - 4;
-                var text = input.Substring(textStartIndex, textEndIndex - textStartIndex);
-                input = input.Replace(tag, string.Format("[{0}]({1})", text, url));
-            }
+            //while (true)
+            //{
+            //    if (!input.Contains(@"<a href="))
+            //    {
+            //        Console.WriteLine(input);
+            //        break;
+            //    }
+            //    var tagStartIndex = input.IndexOf(@"<a href=");
+            //    var tagEndIndex = input.IndexOf(@"</a>") + 4;
+            //    var tag = input.Substring(tagStartIndex, tagEndIndex - tagStartIndex);
+
+            //    var urlStartIndex = input.IndexOf(@"<a href=") + 9;
+            //    var urlEndIndex = input.IndexOf(@""">");
+            //    var url = input.Substring(urlStartIndex, urlEndIndex - urlStartIndex);
+
+            //    var textStartIndex = urlEndIndex + 2;
+            //    var textEndIndex = tagEndIndex - 4;
+            //    var text = input.Substring(textStartIndex, textEndIndex - textStartIndex);
+            //    input = input.Replace(tag, string.Format("[{0}]({1})", text, url));
+            //}
         }
     }
 }
