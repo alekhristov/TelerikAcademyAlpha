80d57401ff2ea0863a95f87338c1cb06528627de
Alek Hristov <alek.hristov@yahoo.com>
2017-11-27 01:27:03 +0200

    EntranceExam will appear on top now

---
+++ 00-EntranceExam/02-CrookedStairs/02-CrookedStairs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CrookedStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstBrickHeight = long.Parse(Console.ReadLine());
+            var secondBrickHeight = long.Parse(Console.ReadLine());
+            var thirdBrickHeight = long.Parse(Console.ReadLine());
+            var numberOfLayers = int.Parse(Console.ReadLine());
+            int numberOfBricks = 0;
+            var listOfBricks = new List<long>();
+            listOfBricks.Add(firstBrickHeight);
+            listOfBricks.Add(secondBrickHeight);
+            listOfBricks.Add(thirdBrickHeight);
+
+
+            for (int i = numberOfLayers; i > 0; i--)
+            {
+                numberOfBricks += i;
+            }
+
+            long nextBrickHeight = 0;
+
+            for (int i = 3; i < numberOfBricks; i++)
+            {
+                nextBrickHeight = firstBrickHeight + secondBrickHeight + thirdBrickHeight;
+                listOfBricks.Add(nextBrickHeight);
+                firstBrickHeight = secondBrickHeight;
+                secondBrickHeight = thirdBrickHeight;
+                thirdBrickHeight = nextBrickHeight;
+            }
+
+            int counter = 0;
+
+            for (int i = 1; i <= numberOfLayers; i++)
+            {
+                string currentLine = string.Empty;
+                for (int k = 0; k < i; k++)
+                {
+                    currentLine += listOfBricks[counter] + " ";
+                    counter++;
+                }
+                currentLine = currentLine.TrimEnd();
+                Console.WriteLine(currentLine);
+            }
+        }
+    }
+}
