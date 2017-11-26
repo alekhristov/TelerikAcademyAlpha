3ed6d54a29d3da3091229b95e1e2d570e09288a7
Alek Hristov <alek.hristov@yahoo.com>
2017-11-26 20:37:44 +0200

    SeriesOfLetters solution

---
+++ 02-Strings/23-SeriesOfLetters/23-SeriesOfLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _23_SeriesOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower();
+            string result = input[0].ToString();
+            var resultIndex = 0;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != result[resultIndex])
+                {
+                    result += input[i];
+                    resultIndex++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
