58810b033074a10c72b0df13a58990c5c4298d39
Alek Hristov <alek.hristov@yahoo.com>
2017-12-07 18:54:46 +0200

    OOP Task

---
+++ ClassesPart1/01-DefineClass/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_DefineClass
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            GSMTest.DisplayGSMsInformation();
+            GSMTest.DisplayIphone4SInformation();
+
+            GSMCallHistoryTest.DisplayCallInfo();
+            GSMCallHistoryTest.CalculateCallPrices(0.37);
+        }
+    }
+}
