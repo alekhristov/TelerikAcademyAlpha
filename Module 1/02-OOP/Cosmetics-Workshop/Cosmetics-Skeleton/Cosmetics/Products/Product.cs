90a1a80763608f17d63b0d792e2f0de8ba41d655
Alek Hristov <alek.hristov@yahoo.com>
2017-12-08 19:50:23 +0200

    Commit all

---
+++ Module 1/02-OOP/Cosmetics-Workshop/Cosmetics-Skeleton/Cosmetics/Products/Product.cs
@@ -0,0 +1,44 @@
+﻿using Bytes2you.Validation;
+using Cosmetics.Common;
+using System;
+
+namespace Cosmetics.Products
+{
+    public class Product
+    {
+        private readonly decimal price;
+        private readonly string name;
+        private readonly string brand;
+        private readonly GenderType gender;
+
+        public Product(string name, string brand, decimal price, GenderType gender)
+        {
+            Guard.WhenArgument(name, "name").IsNull().Throw();
+            Guard.WhenArgument(name.Length, "nameLength").IsLessThan(3).Throw();
+            Guard.WhenArgument(name.Length, "nameLength").IsGreaterThan(10).Throw();
+            Guard.WhenArgument(brand, "brand").IsNull().Throw();
+            Guard.WhenArgument(brand.Length, "brandLength").IsLessThan(2).Throw();
+            Guard.WhenArgument(brand.Length, "brandLength").IsGreaterThan(10).Throw();
+            Guard.WhenArgument(price, "price").IsLessThan(0).Throw();
+            if (!Enum.IsDefined(typeof(GenderType), gender)) throw new ArgumentException("Invalid gender");
+
+            this.name = name;
+            this.brand = brand;
+            this.price = price;
+            this.gender = gender;
+        }
+
+        public decimal Price => price;
+
+        public string Name => name;
+
+        public string Brand => brand;
+
+        public GenderType Gender => gender;
+
+        public string Print()
+        {
+            return $" #{this.Name} {this.Brand}\r\n #Price: ${this.Price}\r\n #Gender: {this.Gender}\r\n ===";
+        }
+    }
+}
