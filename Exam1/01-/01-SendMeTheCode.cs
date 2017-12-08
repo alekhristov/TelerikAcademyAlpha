2540a85f0fa124ed444287439b7eda042c37cc23
Alek Hristov <alek.hristov@yahoo.com>
2017-12-08 19:37:53 +0200

    FirstExam 300/300

--- Exam1/01-/01-SendMeTheCode.cs
+++ Exam1/01-/01-SendMeTheCode.cs
@@ -35,39 +35,6 @@ namespace _01_
                 }
                 index--;
             }
-
-            //var input = Console.ReadLine().ToCharArray();
-            //BigInteger sum = 0;
-            //var index = 1;
-            //var result = string.Empty;
-            //var br = false;
-
-            //for (int i = input.Length - 1; i >= 0; i--)
-            //{
-
-            //    var currentDigit = int.Parse(input[i].ToString());
-
-            //    if (i - 1 == 0 && input[0] == '-')
-            //    {
-            //        currentDigit = currentDigit * (-1);
-            //        br = true;
-            //    }
-
-            //    if (index % 2 != 0)
-            //    {
-            //        sum += currentDigit * index * index;
-            //    }
-            //    else
-            //    {
-            //        sum += currentDigit * currentDigit * index;
-            //    }
-            //    if (br)
-            //    {
-            //        break;
-            //    }
-            //    index++;
-            //}
-
             BigInteger last = sum % 10;
 
             if (last == 0)
