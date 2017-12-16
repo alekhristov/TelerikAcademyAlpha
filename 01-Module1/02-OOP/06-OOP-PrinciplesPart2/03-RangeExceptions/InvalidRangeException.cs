f1181a266b3c3d6992e0dcf10db789954e957d31
Alek Hristov <alek.hristov@yahoo.com>
2017-12-16 09:39:18 +0200

    InvalidRangeException solution

---
+++ 01-Module1/02-OOP/06-OOP-PrinciplesPart2/03-RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_RangeExceptions
+{
+    class InvalidRangeException<T> : Exception
+    {
+        private T min;
+        private T max;
+
+        public InvalidRangeException(T min, T max) : this(null, null, min, max)
+        {
+        }
+
+        public InvalidRangeException(string message, T min, T max) : this(message, null, min, max)
+        {
+        }
+
+        public InvalidRangeException(string message, Exception innerException, T min, T max) : base(message, innerException)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+
+        public T Min { get; }
+
+        public T Max { get; }
+    }
+}
---
+++ 01-Module1/02-OOP/06-OOP-PrinciplesPart2/03-RangeExceptions/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_RangeExceptions
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                TestWithNumbers(1, 100);
+            }
+            catch (InvalidRangeException<int> ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Range: [{ex.Min}...{ex.Max}]");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine();
+                Console.Write("Enter a date: ");
+                TestWithDateTime(new DateTime(1980, 1, 1), new DateTime(2013, 12, 31));
+            }
+            catch (InvalidRangeException<DateTime> ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Range: [{ex.Min.ToString("dd/MM/yyyy")}...{ex.Max.ToString("dd/MM/yyyy")}]");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private static void TestWithDateTime(DateTime minDateTime, DateTime maxDateTime)
+        {
+            var date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            if (date < minDateTime || date > maxDateTime)
+            {
+                throw new InvalidRangeException<DateTime>("Date is out of range", new Exception($"Date must be in Range: [{minDateTime}...{maxDateTime}]"), minDateTime, maxDateTime);
+            }
+        }
+
+        private static void TestWithNumbers(int minNumber, int maxNumber)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            if (number < minNumber || number > maxNumber)
+            {
+                throw new InvalidRangeException<int>("Number out of range", minNumber, maxNumber);
+            }
+        }
+    }
+}
