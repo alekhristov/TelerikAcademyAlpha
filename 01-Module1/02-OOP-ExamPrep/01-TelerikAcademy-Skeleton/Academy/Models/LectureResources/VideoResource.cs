7f2cf282a738944feeace66dfb8bae3ab6ad24bd
Alek Hristov <alek.hristov@yahoo.com>
2017-12-17 13:19:25 +0200

    Task 3 - Done!

---
+++ 01-Module1/02-OOP-ExamPrep/01-TelerikAcademy-Skeleton/Academy/Models/LectureResources/VideoResource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Models.LectureResources
+{
+    class VideoResource : LectureResource
+    {
+        private DateTime uploadedOn;
+
+        public VideoResource(string name, string url) : base(name, url)
+        {
+            this.Type = "Video";
+        }
+
+        public DateTime UploadedOn
+        {
+            get
+            {
+                return this.uploadedOn;
+            }
+            private set
+            {
+                this.uploadedOn = value;
+            }
+        }
+
+        public override string Print()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(base.Print());
+            sb.AppendLine($"     - Uploaded on: {this.UploadedOn}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
