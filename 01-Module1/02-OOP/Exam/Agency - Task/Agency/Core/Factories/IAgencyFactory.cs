ddc32beda06dc6f9da93bc703260221938b9b392
Alek Hristov <alek.hristov@yahoo.com>
2017-12-22 11:28:57 +0200

    AgencyFactory implemented

--- 01-Module1/02-OOP/Exam/Agency - Task/Agency/Core/Factories/IAgencyFactory.cs
+++ 01-Module1/02-OOP/Exam/Agency - Task/Agency/Core/Factories/IAgencyFactory.cs
@@ -5,9 +5,11 @@ namespace Agency.Core.Contracts
 {
     public interface IAgencyFactory
     {
-        IBus CreateBus(int passengerCapacity, decimal pricePerKilometer);
-        
-        ITrain CreateTrain(int passengerCapacity, decimal pricePerKilometer, int carts);
+        IVehicle CreateBus(int passengerCapacity, decimal pricePerKilometer);
+
+        IVehicle CreateAirplane(int passengerCapacity, decimal pricePerKilometer, bool hasFreeFood);
+
+        IVehicle CreateTrain(int passengerCapacity, decimal pricePerKilometer, int carts);
 
         IJourney CreateJourney(string startingLocation, string destination, int distance, IVehicle vehicle);
 
