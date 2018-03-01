8310beb670f0701c66b52d8202075c5e33996b26
Alek Hristov <alek.hristov@yahoo.com>
2018-03-01 02:32:54 +0200

    Videos added

--- TeamProjects/SinglePageApplication/RoadToTheFutureWithSpaceX/app/scripts/rocketsController.js
+++ TeamProjects/SinglePageApplication/RoadToTheFutureWithSpaceX/app/scripts/rocketsController.js
@@ -4,7 +4,7 @@ const rocketsController = (() => {
             .then((result) => {
                 container.database.rockets = result;
 
-                let rocketsData = '<div><h2 class="rockets-header">Random Header</h2><div class="row">';
+                let rocketsData = '<div><h2 class="rockets-header">SpaceX\'s babies</h2><div class="row">';
 
                 for (rocket of result) {
                     rocketsData += `<div id="${rocket.id}" class="col-md-4 cols-style"><a href="#/rockets/${rocket.id}"><img src="app/images/${rocket.id}.jpg" alt="" class="img-fluid img-hover"></a>`
--- TeamProjects/SinglePageApplication/RoadToTheFutureWithSpaceX/app/scripts/videosController.js
+++ TeamProjects/SinglePageApplication/RoadToTheFutureWithSpaceX/app/scripts/videosController.js
@@ -1,18 +1,24 @@
 const videosController = (() => {
     const visualizeVideosController = () => {
         $('#request').html($(`
-            <h2 class="rockets-header">Videos</h2>
+            <h2 class="videos-header">Videos</h2>
             <div class="row">
-                <div class="col-md-6 cols-style">
+                <div class="col-md-4 cols-style">
                     <div class="videos row">
                         <iframe allowfullscreen="" frameborder="0" height="253" scrolling="no" src="https://www.youtube.com/embed/ujX6CuRELFE?rel=0" width="450"></iframe>
                     </div>
                 </div>    
-                <div class="col-md-6 cols-style">
+                <div class="col-md-4 cols-style">
                     <div class="videos row">
                         <iframe allowfullscreen="" frameborder="0" height="253" scrolling="no" src="https://www.youtube.com/embed/Ndpxuf-uJHE?rel=0" width="450"></iframe>
                     </div>
                 </div>
+                <div class="col-md-4 cols-style">
+                <div class="videos row">
+                    <iframe allowfullscreen="" frameborder="0" height="253" scrolling="no" src="https://www.youtube.com/embed/aBr2kKAHN6M" width="450"></iframe>
+                </div>
+            </div>
+                <h4 class="more-videos"><a href="https://www.youtube.com/spacex">View more SpaceX videos</a><h4>
             </div>
         `));
     }
